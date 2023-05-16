6de57152de019a2ca4277a69ebf4c40929fe141e
donny <donny.rolanda@gmail.com>
2023-05-16 15:30:52 +0800

    Ondevelop
    - Improve convert to number

--- helper/ParseFloat.ts
+++ helper/ParseFloat.ts
@@ -1,7 +1,17 @@
 import { toNumber } from "lodash";
 
-export default function (input: any, fixed = 0) {
-  
-  let result = parseFloat(toNumber(input).toFixed(fixed));
-  return result;
+export default function (input: any, fixed?: number | null) {
+  try {
+    if (fixed == null) {
+      let _input = input as string;
+      if (_input.startsWith('-') && _input.length == 1) {
+        return input;
+      }
+      return parseFloat(toNumber(input).toFixed());
+    }
+    let result = parseFloat(toNumber(input).toFixed(fixed));
+    return result;
+  } catch (ex) {
+    return toNumber(input);
+  }
 }
