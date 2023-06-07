3c75aeb438f4b5e808e03211da0cce8e14316b80
donny <donny.rolanda@gmail.com>
2023-06-07 08:10:17 +0800

    Ondevelop
    - improve cookieUtil

--- helper/CookieUtils.ts
+++ helper/CookieUtils.ts
@@ -1,30 +1,32 @@
 interface CookieOptions {
-    maxAge?: number;
-    expires?: Date;
-    path?: string;
-    domain?: string;
-    sameSite?: 'Strict' | 'Lax' | 'None';
-    httpOnly?: boolean;
-    secure?: boolean;
+  maxAge?: number;
+  expires?: Date;
+  path?: string;
+  domain?: string;
+  sameSite?: 'Strict' | 'Lax' | 'None';
+  httpOnly?: boolean;
+  secure?: boolean;
+}
+
+function setCookieValue(name: string, value: any, options: CookieOptions, overrideOptions?: CookieOptions): string {
+  const mergedOptions = { ...options, ...overrideOptions };
+  const serializedValue = typeof value === 'string' ? value : JSON.stringify(value);
+  const cookie = `${name}=${encodeURIComponent(serializedValue)}; ${serializeCookieOptions(mergedOptions)}`;
+  return cookie;
+}
+
+function parseCookieValue(request: Request, name: string) {
+  const cookieHeader = request.headers.get('cookie');
+  if (!cookieHeader) {
+    return null;
   }
-  
-  function setCookieValue(name: string, value: any, options: CookieOptions): string {
-    const serializedValue = JSON.stringify(value);
-    const cookie = `${name}=${encodeURIComponent(serializedValue)}; ${serializeCookieOptions(options)}`;
-    return cookie;
-  }
-  
-  function parseCookieValue(request: Request, name: string): any | null {
-    const cookieHeader = request.headers.get('cookie');
-    if (!cookieHeader) {
-      return null;
-    }
-  
-    const cookies = cookieHeader.split(';');
-    for (const cookie of cookies) {
-      const [cookieName, cookieValue] = cookie.split('=');
-      if (cookieName.trim() === name) {
-        const decodedValue = decodeURIComponent(cookieValue.trim());
+  const cookies = cookieHeader.split(';');
+  for (const cookie of cookies) {
+    const [cookieName, cookieValue] = cookie.split('=');
+    if (cookieName.trim() === name) {
+      const decodedValue = decodeURIComponent(cookieValue.trim());
+      // Check if the value is a valid JSON string
+      if (decodedValue.startsWith('{') && decodedValue.endsWith('}')) {
         try {
           const parsedValue = JSON.parse(decodedValue);
           return parsedValue;
@@ -33,49 +35,72 @@ interface CookieOptions {
           return null;
         }
       }
+      // Check if the value is a valid JSON array string
+      if (decodedValue.startsWith('[') && decodedValue.endsWith(']')) {
+        try {
+          const parsedValue = JSON.parse(decodedValue);
+          return parsedValue;
+        } catch (error) {
+          console.error(`Failed to parse cookie value for ${name}:`, error);
+          return null;
+        }
+      }
+      // If not a JSON string, return the value as is
+      return decodedValue;
     }
-  
-    return null;
   }
-  
-  function serializeCookieOptions(options: CookieOptions): string {
-    const cookieOptions = [];
-  
-    if (options.path) {
-      cookieOptions.push(`Path=${options.path}`);
-    }
-  
-    if (options.expires) {
-      cookieOptions.push(`Expires=${options.expires.toUTCString()}`);
-    }
-  
-    if (options.maxAge) {
-      cookieOptions.push(`Max-Age=${options.maxAge}`);
-    }
-  
-    if (options.domain) {
-      cookieOptions.push(`Domain=${options.domain}`);
-    }
-  
-    if (options.sameSite) {
-      cookieOptions.push(`SameSite=${options.sameSite}`);
-    }
-  
-    if (options.httpOnly) {
-      cookieOptions.push('HttpOnly');
-    }
-  
-    if (options.secure) {
-      cookieOptions.push('Secure');
-    }
-  
-    return cookieOptions.join('; ');
+
+  return null;
+}
+
+
+function serializeCookieOptions(options: CookieOptions): string {
+  const {
+    maxAge = undefined,
+    expires = new Date(Date.now() + 60 * 60 * 1000),
+    path = '/',
+    domain = undefined,
+    sameSite = 'Strict',
+    httpOnly = true,
+    secure = false
+  } = options;
+
+  const cookieOptions = [];
+
+  if (path) {
+    cookieOptions.push(`Path=${path}`);
   }
-  
-  const CookieUtils = {
-    parseCookieValue,
-    setCookieValue
-  };
-  
-  export default CookieUtils;
-  
+
+  if (expires) {
+    cookieOptions.push(`Expires=${expires.toUTCString()}`);
+  }
+
+  if (maxAge) {
+    cookieOptions.push(`Max-Age=${maxAge}`);
+  }
+
+  if (domain) {
+    cookieOptions.push(`Domain=${domain}`);
+  }
+
+  if (sameSite) {
+    cookieOptions.push(`SameSite=${sameSite}`);
+  }
+
+  if (httpOnly !== undefined) {
+    cookieOptions.push(`HttpOnly=${httpOnly}`);
+  }
+
+  if (secure) {
+    cookieOptions.push('Secure');
+  }
+
+  return cookieOptions.join('; ');
+}
+
+const CookieUtils = {
+  parseCookieValue,
+  setCookieValue
+};
+
+export default CookieUtils;
